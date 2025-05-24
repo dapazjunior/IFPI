206bfc9ae4728bacf1e8eebbde6a2f42edc37c3b
dapazjunior <61951645+dapazjunior@users.noreply.github.com>
2025-05-24 07:29:52 -0300

    atividade de html e modificação em questão de PEC

--- documentosGithub/PEC/Exercicios/Semana 08/s08t2q02_somar_digitos.py
+++ documentosGithub/PEC/Exercicios/Semana 08/s08t2q02_somar_digitos.py
@@ -14,20 +14,19 @@ def main():
 def separar_e_somar_algarismos(n):
     """Recebe um número de até 6 dígitos e retorna a soma dos seus algarismos."""
 
-    cm = n // 100000
-    dm = (n % 100000) // 10000
-    um = ((n % 100000) % 10000) // 1000
-    c = (((n % 100000) % 10000) % 1000) // 100
-    d = ((((n % 100000) % 10000) % 1000) % 100) // 10
-    u = ((((n % 100000) % 10000) % 1000) % 100) % 10
+    dm = n // 10000
+    um = (n % 10000) // 1000
+    c = ((n % 10000) % 1000) // 100
+    d = (((n % 10000) % 1000) % 100) // 10
+    u = (((n % 10000) % 1000) % 100) % 10 
 
-    return cm + dm + um + c + d + u
+    return dm + um + c + d + u
 
 
 def verificar_soma(n):
     """Verifica se 'n' está entre 0 e 100000 e retorna a soma dos algarismos. Caso contrário, retorna -1."""
     
-    if 0 <= n <= 100000:
+    if 0 < n < 100000:
         soma = separar_e_somar_algarismos(n)
     else:
         soma = -1
